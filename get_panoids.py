8c86bd5da9406870962666c24c076653b64ff573
tommy <tommy951113@sohu.com>
2019-05-13 20:08:14 +0800

    本文件功能改为GPS坐标转换为腾讯坐标

--- get_panoids.py
+++ get_panoids.py
@@ -1,28 +1,36 @@
 import requests
 import time
 
-# load coordinates 
+# load coordinates
 pointlist = []
-web_service = "https://apis.map.qq.com/ws/streetview/v1/getpano"
+# web_service = "https://apis.map.qq.com/ws/streetview/v1/getpano"
+web_service = "https://apis.map.qq.com/ws/coord/v1/translate"
 radius = 50
-key = "4QVBZ-2YNLO-JZNWX-SALX6-BKVSH-VBFUX"
-panoid_list = []
+KEY = "4QVBZ-2YNLO-JZNWX-SALX6-BKVSH-VBFUX"
+# panoid_list = []
 point_list = []
 
-with open("G:\\research_data\\ruijin_road\\sample_points_translated.txt") as f:
-    for index,line in enumerate(f):
+def write_to_file(locations):
+    time.sleep(0.2)
+    url = web_service + "?locations=" + locations + "&type=3&key=" + KEY
+    r = requests.get(url)
+    locations_translated = r.json()['locations'] 
+    with open("nanjing_point_tencent.txt", "a") as f:
+        for point in locations_translated:
+            f.write(
+                str(point['lat']) + ',' +
+                str(point['lng']) + '\n')
+
+with open("nanjing_point2.txt") as f:
+    for line in f:
         point = {}
-        point['x'] = line.rsplit(',')[0][:10]
-        point['y'] = line.rsplit(',')[1][:9]
+        point['x'] = line.rsplit(',')[1] 
+        point['y'] = line.rsplit(',')[0]
         pointlist.append(point)
 
+locations = ''
 for index, point in enumerate(pointlist):
-    time.sleep(0.25)
-    url = web_service + "?location=" + point['x'] + ',' + point['y'] + "&radius=" + str(radius) + "&key=" + key
-    r = requests.get(url)
-    detail = r.json()['detail']
-    panoid_list.append(detail)
-
-with open("panoid_ruijinlu.txt","w") as f:
-    for panoid in panoid_list:
-        f.write(str(panoid['location']['lat']) + ',' + str(panoid['location']['lng']) + ',' +panoid['id'] + '\n')
+    locations += point['x'][:-1] + ',' + point['y'] + ';'
+    if (index + 1) % 42 == 0:
+        write_to_file(locations[:-1])
+        locations = ''
