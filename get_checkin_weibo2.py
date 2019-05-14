d854d9867c5a6e1569cd594def2a2240130f17e8
tommy <tommy951113@sohu.com>
2019-05-14 21:29:20 +0800

    修改获取微博数据的代码

--- get_checkin_weibo2.py
+++ get_checkin_weibo2.py
@@ -1,10 +1,10 @@
+# -*- coding: utf-8 -*-
 import requests
 import codecs
 import re 
+import time
 
-poi_list = []
-POI_ID = 'B2094655D665A4FC4792'
-API_URL = 'https://m.weibo.cn/api/container/getIndex?containerid=100101' + POI_ID
+API_URL = 'https://m.weibo.cn/api/container/getIndex?containerid=100101'
 TAIL = '_-_weibofeed'
 
 def remove_links(text):
@@ -13,24 +13,52 @@ def remove_links(text):
     return text
 
 # 获取单个poi的所有微博内容
-i = 0
-weibo_list = []
-while True:
-    i += 1
-    r = requests.get(API_URL + TAIL + '&page=' + str(i))
-    if r.json()['ok'] == 0:
-        break
-    result = r.json()
-    card_group = result['data']['cards'][0]['card_group']
-    for card in card_group:
-        text = card['mblog']['text']
-        text = remove_links(text)
-        if text:
-            weibo_list.append(text)
+def get_weibo(poiid):
+    i = 0
+    weibo_list = []
+    # 分页读取微博内容
+    while True:
+        flag = 0
+        time.sleep(0.2)
+        i += 1
+        r = requests.get(API_URL + poiid + TAIL + '&page=' + str(i))
+        result = r.json()
+        # 排除返回空网址，或返回此点已经删除的情况
+        if result['ok'] == 0:
+            break
+        if result['data']['pageInfo']['page_title'] == '此点已经删除':
+            break
+        card_group = result['data']['cards'][0]['card_group']
+        for card in card_group:
+            if card['card_type'] == 9:
+                text = card['mblog']['text']
+                text = remove_links(text)
+                if text:
+                    weibo_list.append(text)
+            else:
+                flag = 1
+                break
+        if flag == 1:
+            break
+    
+    if i == 1:
+        with open('weiboresult.txt','a') as f:
+            f.write(poiid + '\n')
+    else:
+        with codecs.open('weiboresult.txt','a','utf-8') as f:
+            f.write(poiid + ',')
+            for weibo in weibo_list:
+                f.write(weibo + ';')
+            f.write('\n')
 
-if i == 1:
-    print('地点已被删除')
-else:
-    with codecs.open('B2094655D76BA6FE409C.txt','w','utf-8') as f:
-        for weibo in weibo_list:
-            f.write(weibo + '\n')
+# 批量读取poiid并获取微博数据
+poi_list = []
+with open('poiid_fillterd4.txt') as f:
+    for line in f:
+        poiid = line.rsplit(',')[0]
+        poi_list.append(poiid)
+
+poi_list = poi_list[119:]
+
+for poiid in poi_list:
+    get_weibo(poiid)
